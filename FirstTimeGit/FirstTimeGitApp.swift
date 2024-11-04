472c4931f7ff2ada62d18ca771288363d3378bb6
Lorenzo Mazza <lorenzo.mazza001@studenti.uniparthenope.it>
2024-11-04 15:18:54 +0100

    Initial Commit

---
+++ FirstTimeGit/FirstTimeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTimeGitApp.swift
+//  FirstTimeGit
+//
+//  Created by Lorenzo Mazza on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstTimeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
